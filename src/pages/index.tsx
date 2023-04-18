f9cdec767d10784e60c1885aa21b9e6dc0c6eb39
RichardT <chardiecode@gmail.com>
2023-04-18 11:30:50 +0800

    Finished sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,11 @@ const Homepage = () => {
   return (
     <div className="flex h-screen w-full flex-col">
       {/* Header */}
-      <header className="border-gray-380 flex h-16 w-full flex-row items-center justify-around border-b bg-white py-4">
+      <header className="border-gray-380 flex h-16 w-full flex-row items-center justify-around border-b bg-white py-8">
         <div>
           <IoReorderThreeOutline className="text-3xl text-gray-600" />
         </div>
-        <div className="text-lg font-thin">Ultimate Blog App</div>
+        <div className="text-xl font-extrabold">Sk Curvada</div>
         <div className="flex items-center space-x-4">
           <div>
             <BsBell className="text-xl text-gray-600" />
@@ -31,7 +31,7 @@ const Homepage = () => {
         </div>
       </header>
       {/* /Header */}
-      <section className="grid h-full w-full grid-cols-12 place-items-center">
+      <section className="grid w-full grid-cols-12">
         <main className="col-span-8 h-full w-full border-r border-gray-300 px-10 py-5">
           <div className="flex w-full flex-col space-y-4">
             <div className="flex w-full items-center space-x-4">
@@ -82,8 +82,9 @@ const Homepage = () => {
                 <div className="flex">
                   <div className="h-8 w-8 rounded-full bg-gray-500"></div>
                   <div className="ml-2">
-                    <p className="text-xs font-semibold">
-                      ChardieCode &#x2022; April 16, 2023
+                    <p className="text-xs">
+                      <span className="font-semibold">Aya Poarch</span> &#x2022;{" "}
+                      <span className="text-gray-500">April 16, 2023</span>
                     </p>
                     <p className="text-xs text-gray-500">
                       Father, Founder, teacher and software developer
@@ -126,7 +127,67 @@ const Homepage = () => {
             ))}
           </div>
         </main>
-        <aside className="col-span-4 h-full w-full">This is the sidebar</aside>
+        <aside className="col-span-4 flex flex-col space-y-4 p-6">
+          <div className="sticky top-6">
+            <div className="space-y-3 border-b border-b-gray-300 pb-7">
+              <h3 className="mb-5 text-sm">
+                People you might be interested in
+              </h3>
+              <div className="flex flex-col space-y-4">
+                {Array.from({ length: 4 }).map((_, i) => (
+                  <div className="flex flex-row items-center space-x-4">
+                    <div className="h-8 w-8 flex-none rounded-full bg-gray-500"></div>
+                    <div>
+                      <div className="text-xs font-bold text-gray-900">
+                        Aya Poarch
+                      </div>
+                      <div className="text-xs text-gray-600">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Deserunt.
+                      </div>
+                    </div>
+                    <div>
+                      <button className="flex items-center space-x-3 rounded border border-gray-300 px-4 py-1 text-xs transition hover:border-gray-900 hover:text-gray-900">
+                        Follow
+                      </button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div>
+              <h3 className="mb-5 mt-2 text-sm">Your reading list</h3>
+              <div className="flex flex-col space-y-6">
+                {Array.from({ length: 4 }).map((_, i) => (
+                  <div
+                    className="group flex items-center space-x-4 text-xs text-gray-600"
+                    key={i}
+                  >
+                    <div className="aspect-square h-full w-2/5 rounded-xl bg-gray-300"></div>
+                    <div className="flex w-3/5 flex-col space-y-2">
+                      <div className="font-bold group-hover:underline">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      </div>
+                      <div>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Aspernatur minus autem commodi.
+                      </div>
+                      <div className="flex w-full items-center space-x-1">
+                        <div className="h-8 w-8 rounded-full bg-gray-300"></div>
+                        <div className="text-xs">
+                          <span className="font-semibold">Aya Poarch</span>{" "}
+                          &#x2022;{" "}
+                          <span className="text-gray-500">April 16, 2023</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </aside>
+        <footer>This is a footer section</footer>
       </section>
     </div>
   );
